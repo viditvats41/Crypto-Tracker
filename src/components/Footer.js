0483387f506a98fba64f8bf1e5fe1cec2e0f13bd
viditvats41 <vatsvidit1234.com>
2024-06-17 18:12:23 +0530

    initial commit

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import linkedinIcon from "../assets/linkedinIcon.png"
+import githubIcon from "../assets/githubIcon.png"
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    // <div>Made with ❤️ in NIET |   
+    <footer>
+		<div id="footer-last-part">
+			<p className="author">
+				Your one spot for all things crypto. <br/>
+				
+			</p>
+			<p className="copyright">Made with ❤️ in NIET &copy; 2024 &nbsp; </p>
+		</div>
+	</footer>
+//  </div>
+  )
+}
+
+export default Footer
